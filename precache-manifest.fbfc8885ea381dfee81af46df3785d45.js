930cd823ef53296dde29c38b970c795469acb74a
mrkyel <kyel100@naver.com>
2020-06-19 09:54:43 +0900

    Updates

---
+++ precache-manifest.fbfc8885ea381dfee81af46df3785d45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031cf7959a3d5bbc322f6645eb253573",
+    "url": "/Magog-People/index.html"
+  },
+  {
+    "revision": "f8e9c514b018d8fd96f7",
+    "url": "/Magog-People/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "cfedcdf2cd207da82d71",
+    "url": "/Magog-People/static/css/main.78f8bde3.chunk.css"
+  },
+  {
+    "revision": "f8e9c514b018d8fd96f7",
+    "url": "/Magog-People/static/js/2.1f3012ce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Magog-People/static/js/2.1f3012ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfedcdf2cd207da82d71",
+    "url": "/Magog-People/static/js/main.5c5fdf1e.chunk.js"
+  },
+  {
+    "revision": "5c0f9bbd56609f55d472",
+    "url": "/Magog-People/static/js/runtime-main.509b7958.js"
+  }
+]);
